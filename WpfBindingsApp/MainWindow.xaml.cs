89a1d3100401c08d0f199d6305d8fe548dd3dd5d
maxefimov <efimov.top.academy@gmail.com>
2024-06-02 11:03:56 +0300

    Добавьте файлы проекта.

---
+++ WpfBindingsApp/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User user = new()
+        {
+            Name = "Sammy",
+            Age = 31,
+            Email = "sammy@yahoo.com"
+        };
+
+        ListData listData = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //this.DataContext = user;
+
+            //Binding binding = new();
+            //binding.ElementName = "txtBox";
+            //binding.Path = new PropertyPath("Text");
+
+            //txtBlock.SetBinding(TextBlock.TextProperty, binding);
+
+            lstBox.DataContext = listData;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //user.Name = "Billy";
+            //user.Langs.Add("Php");
+
+            //MessageBox.Show(user.ToString());
+
+            listData.Empls.Add("sam");
+            string msg = "";
+            foreach (var i in listData.Empls)
+                msg += i;
+            MessageBox.Show(msg);
+        }
+    }
+
+    public class ListData
+    {
+        public ObservableCollection<string> Empls { set; get; } = new() { "bob", "joe" };
+    }
+
+}
---
+++ WpfBindingsApp/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBindingsApp
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string name = null!;
+        private int age;
+        private string? email;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged();
+            }
+        }
+        public string? Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Langs { get; set; } = new() { "C#", "C++", "Java" };
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Email}";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string property = "")
+        {
+            if(PropertyChanged is not null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
